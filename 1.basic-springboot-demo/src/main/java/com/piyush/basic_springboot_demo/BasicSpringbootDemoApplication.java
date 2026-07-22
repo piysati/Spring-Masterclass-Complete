6e343de657ae4662d72745777118e7df6753b286
Piyush Sati <piyushsati0409@gmail.com>
2026-07-22 20:25:02 +0530

    a basic spring boot project, with spring web to learn structure of project

---
+++ 1.basic-springboot-demo/src/main/java/com/piyush/basic_springboot_demo/BasicSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.piyush.basic_springboot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootDemoApplication.class, args);
+	}
+
+}
